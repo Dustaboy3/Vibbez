00914d0cbec7d014a37913f8d04bf84694c3724e
Soki <val@stojan.org>
2022-06-15 22:07:47 +0200

    Header Creation and content fix

---
+++ project/src/js/Header.js
@@ -0,0 +1,23 @@
+import '../css/Header.css';
+import logo from '../img/logo.svg';
+
+function header() {
+    return (
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+                Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                {/* Learn React */}
+            </a>
+        </header>
+    );
+}
+
+export default header;
