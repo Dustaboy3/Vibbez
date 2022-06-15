cfe6c2706de6c9d6548d936a1dbaaa1406004a85
DomenJ <domenjelicj@gmail.com>
2022-06-15 21:44:49 +0200

    creating archicheture

--- project/src/index.js
+++ project/src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Footer from './js/Footer'; //import component
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const header = ReactDOM.createRoot(document.getElementById('header'));
+const footer = ReactDOM.createRoot(document.getElementById('footer'));
+
+header.render(
   <React.StrictMode>
-    <App />
+    <Footer />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+root.render(
+  <React.StrictMode>
+    <Footer />
+  </React.StrictMode>
+);
+
+footer.render(
+  <React.StrictMode>
+    <Footer />
+  </React.StrictMode>
+);
+
+
 reportWebVitals();
